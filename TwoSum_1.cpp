3b88401bd34053807fd234574213e77d61c9193f
grain1101 <grains@qq.com>
2017-01-20 22:16:01 -0800

    1. Two Sum[unordered_map, mapping[key] = value]

---
+++ TwoSum_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+    // 1. Two Sum
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        vector<int> result;
+        unordered_map<int,int> mapping;
+        for(int i=0;i<nums.size();i++){
+            if(mapping.find(target-nums[i]) == mapping.end()){
+                mapping[nums[i]] = i;
+            }
+            else{
+                return {mapping[target-nums[i]], i};
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+
+    Solution s1;
+    s1.twoSum();
+
+
+    return 0;
+}
