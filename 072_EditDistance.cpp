9d4b551449c43cace9f32b8a7c75236e0ab00660
grain1101 <grains@qq.com>
2017-03-01 16:47:34 -0800

    dp;

---
+++ 072_EditDistance.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t;
+        }
+        cout << endl;
+    }
+}
+
+class Solution {
+  public:
+    // 72. Edit Distance
+    int minDistance(string word1, string word2) {
+        int m = word1.length(), n = word2.length();
+        vector<vector<int> > dp(m + 1, vector<int> (n + 1, 0));
+        for (int i = 1; i <= m; i++)
+            dp[i][0] = i;
+        for (int j = 1; j <= n; j++)
+            dp[0][j] = j;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = min(dp[i - 1][j - 1] + 1, min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+            }
+        }
+        show(dp);
+        return dp[m][n];
+    }
+};
+
+int main() {
+    Solution s1;
+    auto result = s1.minDistance("azced", "abcdef");
+    cout << result << endl;
+    return 0;
+}
