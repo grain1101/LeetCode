3cbb27c081cff8a7a66e87ed6ec89cc6f10a7d18
grain1101 <grains@qq.com>
2017-03-06 15:07:28 -0800

    use two aid vector left[l]&right[r] [l.p.r] height[p]>=height[i]

---
+++ 084_LargestRectangleInHistogram.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t;
+        }
+        cout << endl;
+    }
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+  public:
+    // 84. Largest Rectangle in Histogram
+    int largestRectangleArea(vector<int> height) {
+        if (height.size() == 0) return 0;
+        // left[i]: from left to right, the first value(height[left[i]]) >= height[i]
+        // right[i]: from right to left, the first value(height[right[i]]) >= height[i]
+        // stack st: 单调递增栈。
+        vector<int> left(height.size()), right(height.size());
+        stack<int> st;
+        // fill in left array
+        for(int i = 0; i < height.size(); i++){
+            while(st.size() && height[st.top()] >= height[i]) st.pop();
+            left[i] = st.size() ? st.top() + 1 : 0;
+            st.push(i);
+        }
+        // fill in right array
+        for(int i = height.size() - 1; i >= 0; i--){
+            while(st.size() && height[st.top()] >= height[i]) st.pop();
+            right[i] = st.size() ? st.top() - 1 : 0;
+            st.push(i);
+        }
+        // calculate largest rectangle
+        int ans = 0;
+        for(int i = 0; i < height.size(); i++){
+            ans = max(ans, height[i] * (right[i] - left[i] + 1));
+        }
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> nums = {2,1,5,6,2,3};
+    Solution s1;
+    auto result = s1.largestRectangleArea(nums);
+    cout << result << endl;
+    return 0;
+}
