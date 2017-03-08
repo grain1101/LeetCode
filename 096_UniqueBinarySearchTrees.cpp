db6bbd1aa7fa585b8b22dfab2085974c321cf266
grain1101 <grains@qq.com>
2017-03-08 14:39:36 -0800

    dp[i] += dp[j] * dp[i - 1 - j] j:[0,j)

---
+++ 096_UniqueBinarySearchTrees.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+#include <set>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 96. Unique Binary Search Trees
+    int numTrees(int n) {
+
+    }
+};
+
+
+
+
+int main() {
+    string s = "25525511135";
+    Solution s1;
+    auto result =  s1.restoreIpAddresses(s);
+    for(auto r : result)
+        cout << r << endl;
+    cout << endl;
+    //cout << result << endl;
+}
