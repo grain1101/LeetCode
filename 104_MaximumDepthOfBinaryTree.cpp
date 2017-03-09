b3d8ab52afdab6b5da5f8d98e5b5b7a0721852a7
grain1101 <grains@qq.com>
2017-03-09 00:16:17 -0800

    recursive

---
+++ 104_MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+#include <set>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 104. Maximum Depth of Binary Tree
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (root->left == nullptr && root->right == nullptr) return 1;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+
+
+int main() {
+    Solution s1;
+    string str1 = "aabcc";
+    string str2 = "dbbca";
+    string str3 = "aadbbcbcac";
+    auto result = s1.isInterleave(str1, str2, str3);
+    cout << result << endl;
+}
