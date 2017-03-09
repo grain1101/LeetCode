ca5b67b7e934aadb2b8d240337d49843c33fb79a
grain1101 <grains@qq.com>
2017-03-08 23:52:59 -0800

    BFS

---
+++ 102_BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+#include <set>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 102. Binary Tree Level Order Traversal
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == nullptr) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size()){
+            vector<int> level;
+            int n = q.size();
+            while(n--){
+                TreeNode* cur = q.front();
+                q.pop();
+                level.push_back(cur->val);
+                if (cur->left != nullptr) q.push(cur->left);
+                if (cur->right != nullptr) q.push(cur->right);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
+
+
+int main() {
+    Solution s1;
+    string str1 = "aabcc";
+    string str2 = "dbbca";
+    string str3 = "aadbbcbcac";
+    auto result = s1.isInterleave(str1, str2, str3);
+    cout << result << endl;
+}
