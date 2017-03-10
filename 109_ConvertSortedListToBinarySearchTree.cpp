42c4b6f64effb42d0418e382d143d254fadae29e
grain1101 <grains@qq.com>
2017-03-09 20:53:48 -0800

    same as 108. change list to vector.

---
+++ 109_ConvertSortedListToBinarySearchTree.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+#include <set>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 109. Convert Sorted List to Binary Search Tree
+    TreeNode* generateBST(vector<int>& nums, int l, int r){
+        if (l > r) return nullptr;
+        int mid = (l + r) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = generateBST(nums, l, mid - 1);
+        root->right = generateBST(nums, mid + 1, r);
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> nums;
+        if (head == nullptr) return nullptr;
+        while(head){
+            nums.push_back(head->val);
+            head = head -> next;
+        }
+        return generateBST(nums, 0, nums.size() - 1);
+    }
+};
+
+
+int main() {
+    Solution s1;
+    string str1 = "aabcc";
+    string str2 = "dbbca";
+    string str3 = "aadbbcbcac";
+    auto result = s1.isInterleave(str1, str2, str3);
+    cout << result << endl;
+}
