08320d8ecab200b1b3b5de2969b7a99116e1d098
grain1101 <grains@qq.com>
2017-04-28 15:16:08 -0700

    215;Heap Sort;build_max_heap;max_heapify

---
+++ 215_KthLargestElementInAnArray.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+#include <set>
+#include <string>
+#include<list>
+using namespace std;
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+}
+void showV(auto& result){
+    for(auto r : result)
+        cout << r << " ";
+    cout << endl;
+}
+
+class Solution {
+public:
+    // 215. Kth Largest Element in an Array
+    inline int left(int idx) {
+        return (idx << 1) + 1;
+    }
+    inline int right(int idx) {
+        return (idx << 1) + 2;
+    }
+    void max_heapify(vector<int>& nums, int idx) {
+        int largest = idx;
+        int l = left(idx), r = right(idx);
+        if (l < heap_size && nums[l] > nums[largest]) largest = l;
+        if (r < heap_size && nums[r] > nums[largest]) largest = r;
+        if (largest != idx) {
+            swap(nums[idx], nums[largest]);
+            max_heapify(nums, largest);
+        }
+    }
+    void build_max_heap(vector<int>& nums) {
+        heap_size = nums.size();
+        for (int i = (heap_size >> 1) - 1; i >= 0; i--)
+            max_heapify(nums, i);
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        build_max_heap(nums);
+        for (int i = 0; i < k; i++) {
+            swap(nums[0], nums[heap_size - 1]);
+            heap_size--;
+            max_heapify(nums, 0);
+        }
+        return nums[heap_size];
+    }
+private:
+    int heap_size;
+};
+
+int main() {
+    Solution s1;
+    vector<int> nums = {1};
+    int k = 1;
+    auto ans = s1.findKthLargest(nums, k);
+    cout << ans << endl;
+    //showV(ans);
+}
