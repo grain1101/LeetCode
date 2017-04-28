ab6f7acbaaf8c432c288ec80f84c33da36804273
grain1101 <grains@qq.com>
2017-04-28 15:47:12 -0700

    215;HeapSort;build heap; max heapify

--- 215_KthLargestElementInAnArray.cpp
+++ 215_KthLargestElementInAnArray.cpp
@@ -39,6 +39,7 @@ public:
         return (idx << 1) + 2;
     }
     void max_heapify(vector<int>& nums, int idx) {
+        // O(lgN)
         int largest = idx;
         int l = left(idx), r = right(idx);
         if (l < heap_size && nums[l] > nums[largest]) largest = l;
@@ -49,11 +50,14 @@ public:
         }
     }
     void build_max_heap(vector<int>& nums) {
+        // O(N)
         heap_size = nums.size();
+        // the elements in the sub-array A[(n/2+1),n] are all leaves of the tree
         for (int i = (heap_size >> 1) - 1; i >= 0; i--)
             max_heapify(nums, i);
     }
     int findKthLargest(vector<int>& nums, int k) {
+        // heap sort : O(NlogN)
         build_max_heap(nums);
         for (int i = 0; i < k; i++) {
             swap(nums[0], nums[heap_size - 1]);
