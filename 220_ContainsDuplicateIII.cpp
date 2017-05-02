185470397be18cc44120b7c7186456828707fa25
grain1101 <grains@qq.com>
2017-05-02 00:26:49 -0700

    220;set<long long> window; window.lower_bound(a) [a,b)

--- 220_ContainsDuplicateIII.cpp
+++ 220_ContainsDuplicateIII.cpp
@@ -29,19 +29,32 @@ void showV(auto& result){
     cout << endl;
 }
 
-
+struct SwapLine{
+    int x;
+    int y;
+    bool in;
+    SwapLine(int X, int Y, bool In): x(X), y(Y), in(In){};
+    bool operator<(const SwapLine& a) const {
+        if (a.x != x) return x < a.x;
+        if (a.y != y) return y < a.y;
+        return a.in < in;
+    }
+    void show(){
+        cout << x << " " << y << " " << in << endl;
+    }
+};
 class Solution {
 public:
     // 220. Contains Duplicate III
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        set<int> window;
+        set<long long> window;
         for (int i = 0; i < nums.size(); i++) {
             if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
             // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
-            auto pos = window.lower_bound(nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t
+            auto pos = window.lower_bound(nums[i] * 1ll - t); // x-nums[i] >= -t ==> x >= nums[i]-t
             // x - nums[i] <= t ==> |x - nums[i]| <= t
-            if (pos != window.end() && *pos - nums[i] <= t) return true;
-            window.insert(nums[i]);
+            if (pos != window.end() && *pos*1ll - nums[i]*1ll <= t * 1ll) return true;
+            window.insert(nums[i] * 1ll);
         }
         return false;
     }
@@ -49,7 +62,7 @@ public:
 
 int main() {
     Solution s1;
-    vector<int> nums = {0,1,2,3};
+    vector<int> nums = {-2147483648,-2147483647};
     auto ans = s1.containsNearbyAlmostDuplicate(nums,3,3);
     cout << ans << endl;
     //show(ans);
