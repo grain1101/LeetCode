b922e54e7ac24fb37c4b14c1707babd75d5524aa
grain1101 <grains@qq.com>
2017-05-03 21:10:56 -0700

    leetcode data structure

---
+++ leetcode.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+#include <set>
+#include <string>
+#include<list>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+struct UndirectedGraphNode {
+    int label;
+    vector<UndirectedGraphNode *> neighbors;
+    UndirectedGraphNode(int x) : label(x) {};
+};
+
+struct RandomListNode {
+     int label;
+     RandomListNode *next, *random;
+     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+void show(auto& result){
+    for(auto r : result){
+        for(auto t : r){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+}
+
+void showV(auto& result){
+    for(auto r : result)
+        cout << r << " ";
+    cout << endl;
+}
