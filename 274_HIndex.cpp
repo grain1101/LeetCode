880e8a093507ab1f15dadf27870e00182bd57700
grain1101 <grains@qq.com>
2017-05-07 21:32:17 -0700

    274;O(N), Count Sort;

--- 274_HIndex.cpp
+++ 274_HIndex.cpp
@@ -4,8 +4,10 @@ public:
     // 274. H-Index
     int hIndex(vector<int>& citations) {
         if (citations.size() == 0) return 0;
-        sort(citations.begin(), citations.end());
-        int n = citations.size(), ret = 0;
+        countSort(citations);
+        //showV(citations);
+        int n = citations.size();
+        int ret = 0;
         for(int i = n; i >=0; i--){
             if (citations[n - i] >= i && citations[n - i - 1] <= i){
                 ret = i;
@@ -14,11 +16,24 @@ public:
         }
         return ret;
     }
+    void countSort(vector<int>& citations){
+        int n = citations.size();
+        vector<int> cnts(n+1, 0);
+        for(int i = 0; i < n; i++){
+            if (citations[i] >= n) cnts[n]++;
+            else cnts[citations[i]]++;
+        }
+        int j = 0;
+        for(int i = 0; i <= n; i++){
+            for(int k = 0; k < cnts[i]; k++){
+                citations[j++] = i;
+            }
+        }
+    }
 };
-
 int main() {
     Solution s1;
-    vector<int> nums = {1,1,1,1,1,2};
+    vector<int> nums = {12};
     auto ans = s1.hIndex(nums);
     cout << ans << endl;
     //showV(ans);
