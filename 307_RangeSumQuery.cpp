3b139ede09a84292e76767a9068cf8d45b0b2bd5
grain1101 <grains@qq.com>
2017-05-11 23:52:54 -0700

    307;BIT;Binary Indexed Trees;

--- 307_RangeSumQuery.cpp
+++ 307_RangeSumQuery.cpp
@@ -5,33 +5,33 @@ public:
     // 307. Range Sum Query - Mutable
     NumArray(vector<int> nums) {
         this->nums = nums;
-        tree.assign(nums.size(), 0);
+        tree.assign(nums.size() + 1, 0);
         for(int i = 0; i < nums.size(); i++){
-            add(i, nums[i]);
+            add(i + 1, nums[i]);
         }
     }
 
     void update(int i, int val) {
-        add(i, val - nums[i]);
+        add(i + 1, val - nums[i]);
         nums[i] = val;
     }
 
     int sumRange(int i, int j) {
-        return getSum(j) - getSum(i - 1);
+        return getSum(j + 1) - getSum(i);
     }
 private:
     vector<int> tree;
     vector<int> nums;
     int getSum(int i){
         int ret = 0;
-        while(i >= 0){
+        while(i > 0){
             ret += tree[i];
             i -= lowBit(i);
         }
         return ret;
     }
     void add(int i, int val){
-        while(i < nums.size()){
+        while(i < tree.size()){
             tree[i] += val;
             i += lowBit(i);
         }
