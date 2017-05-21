e83678753412b10491397877eaca3adabdb9cfb1
grain1101 <grains@qq.com>
2017-05-21 00:01:14 -0700

    413;traversal;

---
+++ 413_ArithmeticSlices.cpp
@@ -0,0 +1,33 @@
+#include "leetcode.h"
+
+// 413. Arithmetic Slices
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        if (A.size() < 3) return 0;
+        int ret = 0, i = 2, j = 0, distance = A[1] - A[0];
+        for(; i < A.size(); i++){
+            if (A[i] - A[i - 1] == distance) continue;
+            int len = i - j;
+            if (len >= 3){
+                for(int k = len - 2; k >= 0; k--) ret += k;
+            }
+            j = i - 1;
+            distance = A[i] - A[i - 1];
+        }
+        if (i == A.size()){
+            int len = i - j;
+            if (len >= 3){
+                for(int k = len - 2; k >= 0; k--) ret += k;
+            }
+        }
+        return ret;
+    }
+};
+
+int main() {
+    Solution s1;
+    vector<int> nums = {1,2,3,5,7,14,21};
+    auto ans = s1.numberOfArithmeticSlices(nums);
+    cout << ans << endl;
+}
