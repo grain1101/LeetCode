e6d01a26a0d20dcfbf1c6a5b5cb7d5e5a7dae25b
grain1101 <grains@qq.com>
2017-05-24 22:41:27 -0700

    394;recursive;

--- 394_DecodeString.cpp
+++ 394_DecodeString.cpp
@@ -4,60 +4,26 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> nums;
-        stack<string> strs;
+        int len = 0;
+        return dfs(len, s);
+    }
+    string dfs(int &idx, const string& s) {
         string ret;
-        for(int i = 0; i < s.size(); i++){
-            if (isdigit(s[i])){
-                int j = i;
+        for(; idx < s.size(); idx++) {
+            if (isdigit(s[idx])) {
+                int j = idx;
                 while(j < s.size() && isdigit(s[j])) j++;
-                int n = 0;
-                for(int k = i; k < j; k++){
-                    n = n * 10 + s[k] - '0';
+                int c = stoi(s.substr(idx, j - idx));
+                idx = j + 1;
+                string nextret = dfs(idx, s);
+                while(c--) {
+                    ret += nextret;
                 }
-                nums.push(n);
-                i = j - 1;
+            } else if (isalpha(s[idx])){
+                ret.push_back(s[idx]);
             } else {
-                if (s[i] == '[') {
-                    continue;
-                }
-                if (isalpha(s[i])) {
-                    int j = i;
-                    while(j < s.size() && isalpha(s[j])) j++;
-                    string cur = s.substr(i, j - i);
-                    if (nums.size() == strs.size() && nums.size() != 0){
-                        string top = strs.empty() ? "" : strs.top();
-                        if (!strs.empty()) strs.pop();
-                        strs.push(top + cur);
-                    }
-                    else if (nums.empty() && strs.empty()) ret += cur;
-                    else strs.push(cur);
-                    i = j - 1;
-                }
-                if (s[i] == ']'){
-                    int n = nums.empty() ? 0 : nums.top();
-                    if (!nums.empty()) nums.pop();
-                    string str = strs.empty() ? "" : strs.top();
-                    if (!strs.empty()) strs.pop();
-                    string tmp= "";
-                    for(int i = 0; i < n; i++){
-                        tmp += str;
-                    }
-
-                    if (!strs.empty() || !nums.empty()){
-                        string before = strs.empty() ? "" : strs.top();
-                        if (!strs.empty()) strs.pop();
-                        before += tmp;
-                        strs.push(before);
-                    }else {
-                        ret += tmp;
-                    }
-                }
+                return ret;
             }
-        }
-        while(!strs.empty()){
-            ret += strs.top();
-            strs.pop();
         }
         return ret;
     }
