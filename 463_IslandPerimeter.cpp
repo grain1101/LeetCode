114651826c384bbd091ad88f759326a9a58e31e0
grain1101 <grains@qq.com>
2017-05-30 20:42:07 -0700

    463;4*cnt-2*repeat; repeat only count up and left;

--- 463_IslandPerimeter.cpp
+++ 463_IslandPerimeter.cpp
@@ -4,34 +4,18 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        if (grid.size() == 0) return 0;
-        if (grid[0].size() == 0) return 0;
-        this->grid = grid;
-        ret = 0;
+        int cnt = 0, repeat = 0;
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if (grid[i][j]) dfs(i, j);
+                if (grid[i][j]){
+                    cnt++;
+                    // up and left;
+                    if (i > 0 && grid[i - 1][j] == 1) repeat++;
+                    if (j > 0 && grid[i][j - 1] == 1) repeat++;
+                }
             }
         }
-        return ret;
-    }
-private:
-    vector<vector<int>> grid;
-    int ret;
-    vector<vector<int>> d = {{-1,0},{0,1},{1,0},{0,-1}};
-    bool checkBoundary(int p, int q){
-        return p >= 0 && p < grid.size() && q >=0 && q < grid[0].size();
-    }
-    void dfs(int p, int q){
-        if (!checkBoundary(p, q) || grid[p][q] == 0) {
-            ret++;
-            return;
-        }
-        if (grid[p][q] == -1) return;
-        grid[p][q] = -1;
-        for(int i = 0; i < 4; i++){
-            dfs(p + d[i][0], q + d[i][1]);
-        }
+        return 4 * cnt - 2 * repeat;
     }
 };
 
