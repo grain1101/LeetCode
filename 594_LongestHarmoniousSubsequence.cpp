6b03c3f528e13ce3ab7ee654327d9d6e6e098c6b
grain1101 <grains@qq.com>
2017-06-04 23:21:57 -0700

    594;

--- 594_LongestHarmoniousSubsequence.cpp
+++ 594_LongestHarmoniousSubsequence.cpp
@@ -4,27 +4,25 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
-        sort(nums.begin(), nums.end());
+        unordered_map<int, int> m;
         int ret = 0;
-        int j = 0, tmp = nums[0];
-        showV(nums);
-        for(int i = 1; i < nums.size(); i++){
-            if (i != nums.size() - 1 && (nums[i] == nums[i - 1] || nums[i] == tmp + 1)) continue;
-            cout << j << " " << i << " " << tmp << endl;
-            if (nums[i - 1] == tmp + 1) ret = max(ret, i - j);
-            if (nums[i] == tmp + 1) ret = max(ret, i - j + 1);
-            while(nums[j] == tmp) j++;
-            tmp = nums[j];
+        for(auto n : nums){
+            m[n]++;
+            if (m.find(n - 1) != m.end()){
+                ret = max(ret, m[n - 1] + m[n]);
+            }
+            if (m.find(n + 1) != m.end()){
+                ret = max(ret, m[n + 1] + m[n]);
+            }
         }
-        if (nums[nums.size() - 1] == nums[0] + 1) return nums.size();
         return ret;
     }
 };
 
 int main() {
     Solution s1;
-    vector<int> nums = {3,2,2,3,2,1,3,3,3,-2,0,3,2,1,0,3,1,0,1,3,0,3,3};
-    auto ans = s1.findLHS(nums);
+    string s = "abcdefg";
+    int k = 2;
+    auto ans = s1.reverseStr(s, k);
     cout << ans << endl;
 }
