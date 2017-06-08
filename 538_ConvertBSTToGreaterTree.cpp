f5004d183140226e9b1f81f5248aa3bc9a99241a
grain1101 <grains@qq.com>
2017-06-07 21:27:03 -0700

    538;

---
+++ 538_ConvertBSTToGreaterTree.cpp
@@ -0,0 +1,19 @@
+#include "leetcode.h"
+
+// 538. Convert BST to Greater Tree
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+
+    }
+};
+
+
+int main() {
+    Solution2 s1;
+    string s = "abab";
+    auto ans = s1.repeatedSubstringPattern("abadabad");
+    cout << ans << endl;
+
+    int i = 0, j = 0;
+}
