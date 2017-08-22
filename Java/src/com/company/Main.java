8be4d8191393ff311ff106ff3e4593ae5700f110
grain1101 <grains@qq.com>
2017-08-21 23:54:33 -0700

    update

---
+++ Java/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int x = 0;
+        System.out.println(x);
+        Main m = new Main();
+        String s = "hello";
+        System.out.println(s.length());
+    }
+}
